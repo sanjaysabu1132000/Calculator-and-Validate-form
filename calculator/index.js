abf7f0145153de87bfebb2c6ed051c35f7df36ae
sanjaysabu1132000 <sanjaysabu7777@gmail.com>
2024-02-24 00:43:44 +0530

    final

--- calculator/index.js
+++ calculator/index.js
@@ -1,21 +1,31 @@
 function addToDisplay(value) {
-    document.getElementById("display").value += value;
-  }
-  function clearDisplay() {
-    document.getElementById("display").value = "";
-  }
-  
+  document.getElementById("display").value += value;
+}
 
-  function calculate() {
-    try {
-      let result = eval(document.getElementById("display").value);
-      document.getElementById("display").value = result;
-    } catch (error) {
-      document.getElementById("display").value = "error";
-    }
+function clearDisplay() {
+  document.getElementById("display").value = "";
+  document.getElementById("display2").value = "";
+}
+
+// function calculate() {
+//   try {
+//     let result = eval(document.getElementById("display").value);
+//     document.getElementById("display").value = result;
+//   } catch (error) {
+//     document.getElementById("display").value = "error";
+//   }
+// }
+
+function calculate() {
+  try {
+    let result = eval(document.getElementById("display").value);
+    document.getElementById("display2").value = result;
+  } catch (error) {
+    document.getElementById("display2").value = "syntax error";
   }
-  function clearLast() {
-  var display = document.getElementById('display');
+}
+
+function clearLast() {
+  var display = document.getElementById("display");
   display.value = display.value.slice(0, -1);
 }
- 
